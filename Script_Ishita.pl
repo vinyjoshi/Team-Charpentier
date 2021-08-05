65c3b10a9a426638fa89fcbf8e2e34fd3d1cbac4
31ishita12 <88451331+31ishita12@users.noreply.github.com>
2021-08-05 20:08:24 +0530

    Ishita

---
+++ Script_Ishita.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+$name = 'Ishita Shreshtha';
+$email = '31ishita12@gmail.com';
+$slack = '@IshreshthA';
+$biostack = 'Drug Development';
+$twitter = '@IShreshtha';
+
+$distance = 0;
+foreach $i( 0 .. length($slack)) {
+    $distance++ if (substr($slack,$i,1) ne substr($twitter,$i,1));
+  }
+
+print "$name, $email, $slack, $biostack, $twitter, $distance";
