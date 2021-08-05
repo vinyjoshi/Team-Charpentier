8ef90824ce8045c67b354979a00382a536d46931
Anirudh1009 <88363933+Anirudh1009@users.noreply.github.com>
2021-08-05 22:39:39 +0530

    Update Script_anirudh.php

--- Script_anirudh.php
+++ Script_anirudh.php
@@ -1,23 +1,22 @@
 #!/usr/bin/php
-
 <?php
-function hammingDist($str1, $str2)
-{
+
+function hammingDist($str1, $str2){
     $i = 0; $count = 0;
-    while (isset($str1[$i]) != '')
-    { if ($str1[$i] != $str2[$i])
+    while (isset($str1[$i]) != ''){
+        if ($str1[$i] != $str2[$i])
             $count++;
-        $i++;}
+        $i++;
+    }
     return $count;
 }
+
 $Name = 'Anirudh Arun';
 $Email = 'anirudhau.1009@gmail.com';
 $SlackUsername = '@Anirudh';
 $Biostack = 'Drug Development';
 $Twitter = "@anirudH";
-
 $Distance = hammingDist($SlackUsername, $Twitter);
 
 echo "$Name,$Email,$SlackUsername,$Biostack,$Twitter,$Distance";
-
 ?>
