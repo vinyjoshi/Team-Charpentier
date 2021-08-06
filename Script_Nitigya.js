e66d303cbd02815b08450ac9531097d8776cc870
Nitigya Mathur <68492300+NitigyaM@users.noreply.github.com>
2021-08-06 23:39:06 +0530

    Script_Nitigya.js

---
+++ Script_Nitigya.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+function hammingDist(str1, str2)
+{
+    let i = 0, count = 0;
+    while (i < str1.length)
+    {
+        if (str1[i] != str2[i])
+            count++;
+        i++;
+    }
+    return count;
+}
+    var Name = "Nitigya";
+    var Email = "nitigyaM@something.com";
+    var Slackusername = "@Nitigya-M";
+    var Biostack = "Nitibiostack";
+    var Twitter = "@Nitigya_M";
+
+    console.log(Name +',' + Email +','+ Slackusername +','+ Biostack + ","+ Twitter+ ","+ hammingDist (Slackusername, Twitter));
