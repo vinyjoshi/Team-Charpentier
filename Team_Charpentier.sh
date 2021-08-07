1ebfa2991fe65d7e6e12126900e28a9364c63f18
Vinay Joshi <60395756+vinyjoshi@users.noreply.github.com>
2021-08-07 20:11:16 +0530

    Update Team_Charpentier.sh

--- Team_Charpentier.sh
+++ Team_Charpentier.sh
@@ -9,11 +9,6 @@ cd ./Team-Charpentier
 # Before running these file, we have to make them executable, this command give the file executable rights.
 chmod +x Script*
 
-# These file were mostly written in Windows, so when running these scripts in Linux, they give an 'bad interpreter' error.
-# This occurs due to the different conventions used in Window and Linux when going to a new line. 
-# In Windows it is ("\r\n") whereas in Linux its just ("\n"). This command resolves that issue.
-dos2unix Script*
-
 # Looping through all the scripts one-by-one
 for i in $(ls Script*)
 do
