3ed0a65636239f6b39bbdc3708ead9926fd0ba5d
HarshithaBC <88304354+HarshithaBC@users.noreply.github.com>
2021-08-07 19:44:03 +0530

    Harshitha

---
+++ Script_Harshitha.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+​
+def hammingDist(str1, str2):
+    i = 0
+    count = 0
+    while(i < len(str1)):
+        if(str1[i] != str2[i]):
+            count += 1
+        i += 1
+    return count
+    
+name="Harshitha BC"
+email="harshitha.chandra01@gmail.com"
+slackusername="@Harshitha"
+biostack="Genomics"
+Twitter="@Harshiibc"
+​
+distance = hammingDist(slackusername, Twitter)    
+print('{},{},{},{},{},{}'.format(name,email,slackusername,biostack,Twitter,distance))
